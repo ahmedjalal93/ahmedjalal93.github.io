5c2238a17408b7229b489b13f32a055a56b91504
Ahmed Jalal <jalal.ahmed@orbcomm.com>
2020-05-02 22:26:12 -0600

    Initial commit

---
+++ work7/data.js
@@ -0,0 +1,16 @@
+class Data {
+    constructor (data){
+        this.username = data.username;
+        this.email = data.email;
+        this.project = data.project;
+        this.url = data.url;
+        this.description = data.description;
+        this.license = data.license;
+        this.install = data.install;
+        this.test = data.test;
+        this.about = data.about;
+        this.contribution = data.contribution;
+    }
+}
+
+module.exports = Data;
---
+++ work7/utils/generateMarkdown.js
@@ -0,0 +1,40 @@
+function generateMarkdown(data) {
+  return `
+# ${data.project}
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](${data.url})
+
+## Description
+ ${data.description}
+
+# Table of Content
+* [Installation](#installation)
+* [Usage](#uage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run the following command: 
+${data.install}
+
+## Usage
+${data.about}
+
+## License
+This project is licensed under the MIT license
+
+## Contributing
+${data.contribution}
+
+## Tests
+To run tests, run the following command:
+${data.test}
+
+## Questions
+if you have any questions contact ${data.username} at ${data.email}
+
+`;
+}
+
+module.exports = generateMarkdown;
